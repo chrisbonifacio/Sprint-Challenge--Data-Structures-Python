60e49411014a97f083770a13a52510e6e50270a1
Chris Bonifacio <christopher.bonifacio@gmail.com>
2020-02-03 22:42:11 -0500

    wrote solution for names

--- names/names.py
+++ names/names.py
@@ -10,11 +10,23 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+all_names = names_1 + names_2
+all_names.sort()
+
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+for i in range(len(all_names) - 1):
+    if all_names[i] == all_names[i + 1]:
+        duplicates.append(all_names[i])
+
+duplicates = list(set(duplicates))
+duplicates.sort()
+
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
